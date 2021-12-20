80bd485ed723aa77a886b5ae8923cf449cf2d57f
Lonewolfrex <samparkprakash27@gmail.com>
2021-12-20 21:15:40 +0530

    Fresh code

---
+++ LocalLiquor1/src/main/java/LocalLiquor1/LocalLiquor1/LiquorService.java
@@ -0,0 +1,30 @@
+package LocalLiquor1.LocalLiquor1;
+
+import LocalLiquor1.LocalLiquor1.LiquorType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LiquorService {
+
+    public List getAvailableBrands(LiquorType type){
+
+        List brands = new ArrayList();
+
+        if(type.equals(LiquorType.WINE)){
+            brands.add("Adrianna Vineyard");
+            brands.add(("J. P. Chenet"));
+
+        }else if(type.equals(LiquorType.WHISKY)){
+            brands.add("Glenfiddich");
+            brands.add("Johnnie Walker");
+
+        }else if(type.equals(LiquorType.BEER)){
+            brands.add("Corona");
+
+        }else {
+            brands.add("No Brand Available");
+        }
+    return brands;
+    }
+}
---
+++ LocalLiquor1/src/main/java/LocalLiquor1/LocalLiquor1/LiquorType.java
@@ -0,0 +1,5 @@
+package LocalLiquor1.LocalLiquor1;
+
+public enum LiquorType {
+	BEER, WINE, WHISKY, RUM, VODKA
+}
