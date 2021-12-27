4ee18e8124b582fff0e4c3e444399aa482ccfd4e
Lonewolfrex <samparkprakash27@gmail.com>
2021-12-27 20:15:52 +0530

    updated web.xml

--- BasicWebApplication/src/BasicWebApplication.java
+++ BasicWebApplication/src/BasicWebApplication.java
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet("/TestServlet")
-public class TestServlet extends HttpServlet {
+@WebServlet("/BasicWebApplication")
+public class BasicWebApplication extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TestServlet() {
+    public BasicWebApplication() {
         super();
         // TODO Auto-generated constructor stub
     }
